0a03db83a1f71dc37843db555651dfc1b9d3890b
mjsouthernn <104192810+mjsouthern@users.noreply.github.com>
2025-03-19 15:31:57 +0800

    Deleted folder 2

--- structures2/tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
-
-    for(int i=0; i<2 ; i++) {
-        printf("\n------- Record -------\n");
-        printf("First Name: %s\n", p1[i].fname);
-        printf("Middle Name: %s\n", p1[i].mname);
-        printf("Last Name: %s\n", p1[i].lname);
-        printf("Address: %s\n", p1[i].address);
-        printf("Age: %d\n", p1[i].age);
-        printf
